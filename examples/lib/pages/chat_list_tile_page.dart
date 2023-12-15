10a627a99b6d8cab598fb2ab291eead475934782
NgurahSuwijaya <80748618+NgurahSuwijaya@users.noreply.github.com>
2023-12-15 14:56:48 +0800

    ad littel update lantern

--- examples/lib/pages/chat_list_tile_page.dart
+++ examples/lib/pages/chat_list_tile_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_common_widgets/core/color_manager.dart';
-import 'package:mobile_common_widgets/core/enum.dart';
 import 'package:mobile_common_widgets/core/text_style_manager.dart';
 import 'package:mobile_common_widgets/list_tile/js_chat_list_tile.dart';
-import 'package:mobile_common_widgets/list_tile/js_notification_list_tile.dart';
 
 class ChatListTilePage extends StatelessWidget {
   const ChatListTilePage({super.key});
@@ -21,27 +19,27 @@ class ChatListTilePage extends StatelessWidget {
             logo:
                 'https://cdn.discordapp.com/attachments/856786757516918784/1152763696142090280/istockphoto-537289161-612x612.jpeg?ex=65877bda&is=657506da&hm=5d1eda32f581e831689c025b4441666883365de09000eb5a5dbae31cfa117e39&',
             title: 'notiftitle',
-            typingStatusWidget: Text('Hallo'),
+            typingStatusWidget: const Text('Hallo'),
             sendTime: '09:59',
             totalNew: '1',
-            unreadStatus: Text('Hallo'),
+            unreadStatus: const Text('Hallo'),
           ),
           JSChatListTile(
             onTap: () {},
             logo:
                 'https://cdn.discordapp.com/attachments/856786757516918784/1152763696142090280/istockphoto-537289161-612x612.jpeg?ex=65877bda&is=657506da&hm=5d1eda32f581e831689c025b4441666883365de09000eb5a5dbae31cfa117e39&',
             title: 'notiftitle',
-            typingStatusWidget: Text('Typing'),
+            typingStatusWidget: const Text('Typing'),
             sendTime: '09:59',
             totalNew: '1',
-            unreadStatus: Text('Hallo'),
+            unreadStatus: const Text('Hallo'),
           ),
           JSChatListTile(
             onTap: () {},
             logo:
                 'https://cdn.discordapp.com/attachments/856786757516918784/1152763696142090280/istockphoto-537289161-612x612.jpeg?ex=65877bda&is=657506da&hm=5d1eda32f581e831689c025b4441666883365de09000eb5a5dbae31cfa117e39&',
             title: 'notiftitle',
-            typingStatusWidget: Text('Typing'),
+            typingStatusWidget: const Text('Typing'),
             sendTime: '09:59',
             totalNew: '1',
             unreadStatus: Container(
